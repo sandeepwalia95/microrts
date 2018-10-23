2d004e3fbfc68508dbf48f67edff984f38fbbbf9
MartaM <marciszewiczm@gmail.com>
2018-10-23 21:52:31 +0100

    generation of a population

--- src/QMSajidWaliaMarciszewicz/Strategies/OEP.java
+++ src/QMSajidWaliaMarciszewicz/Strategies/OEP.java
@@ -1,33 +1,44 @@
 package QMSajidWaliaMarciszewicz.Strategies;
 
+import ai.abstraction.AbstractAction;
 import ai.abstraction.pathfinding.PathFinding;
+import exercise8.PlayerAbstractActionGenerator;
 import rts.GameState;
 import rts.PlayerAction;
+import rts.UnitAction;
+import rts.units.Unit;
 import rts.units.UnitTypeTable;
 import util.Pair;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.PriorityQueue;
 
 public class OEP extends QMStrategy {
 
+    private int idGenerator =0;
     private int TIME_BUDGET;
     private int ITERATIONS_BUDGET;
     private int _populationSize;
 
+    private int _playerID;
+    PlayerAbstractActionGenerator genomesGenerator = null;
+
     //parameter used for selection of parents for new generation
     private int _kparents=0;
+    private int _lookahead; //how far in future are we looking/ how long is the genome
 
     public class Population{
         PriorityQueue<Genome> individuals; //maybe better if some priority queue not just simple list
     }
 
     public class Genome implements Comparable<Genome>{
-        int ID;
-        PlayerAction[] genes;
+        int ID; //maybe id appears to be not needed then we can get rid of it
+        PlayerAction genes;
         GameState phenotype;
         float fitness; //result of fitness function/ evaluation of this sequence
+        HashMap<Unit, AbstractAction> abstractActions = new HashMap<>();
 
         @Override
         public int compareTo(Genome o) {
@@ -45,15 +56,18 @@ public class OEP extends QMStrategy {
 
 
 
-    public OEP(int timeBudget, int iterationBudget, int populationSize){
+    public OEP(int timeBudget, int iterationBudget, int populationSize, int lookahead){
         this.TIME_BUDGET = timeBudget;
         this.ITERATIONS_BUDGET = iterationBudget;
         this._populationSize = populationSize;
+        this._lookahead = lookahead;
     }
 
     @Override
     public PlayerAction execute(int player, GameState gs, UnitTypeTable utt, PathFinding pf) throws Exception {
 
+        _playerID = player;
+        this.genomesGenerator = new PlayerAbstractActionGenerator(utt);
         long start = System.currentTimeMillis();
         int nruns = 0;
 
@@ -61,40 +75,35 @@ public class OEP extends QMStrategy {
         if(gs.canExecuteAnyAction(player))
         {
             //1. Initialize the population for t=0
-            generatePopulation();
-            repairGenomes();
-
-            while(true) {
-                if (TIME_BUDGET>0 && (System.currentTimeMillis() - start)>=TIME_BUDGET) break;
-                if (ITERATIONS_BUDGET>0 && nruns>=ITERATIONS_BUDGET) break;
-
-                //2. Evaluate population
-                evaluatePopulation();
-                //3. Select k individuals for the new population (remove the ones with lowest fitness)
-                PriorityQueue<Genome> parents = selectParents(_kparents);
-                //4. Create pairs from selected individuals
-                ArrayList<Pair<Genome,Genome>> couples = pairIndividuals(parents);
-                //5. Crossover
-                ArrayList<Genome> kids = crossover(couples);
-                //5a. repair
-                kids = repairGenomes(kids);
-                //6. Mutate newly created individuals.
-                kids = mutation(kids);
-                //6a. repair
-                kids = repairGenomes(kids);
-                //7. Create population for t+1
-                _population.individuals = new PriorityQueue<>(parents);
-                _population.individuals.addAll(kids);
-
-                nruns++;
-            }
-
-            /*
-            for(Unit u : gs.getUnits())
+            if(generatePopulation(gs))
             {
-                if(u.getPlayer() == player)
-                    ;
-            }*/
+                repairGenomes();
+
+                while(true) {
+                    if (TIME_BUDGET>0 && (System.currentTimeMillis() - start)>=TIME_BUDGET) break;
+                    if (ITERATIONS_BUDGET>0 && nruns>=ITERATIONS_BUDGET) break;
+
+                    //2. Evaluate population
+                    evaluatePopulation();
+                    //3. Select k individuals for the new population (remove the ones with lowest fitness)
+                    PriorityQueue<Genome> parents = selectParents(_kparents);
+                    //4. Create pairs from selected individuals
+                    ArrayList<Pair<Genome,Genome>> couples = pairIndividuals(parents);
+                    //5. Crossover
+                    ArrayList<Genome> kids = crossover(couples);
+                    //5a. repair
+                    kids = repairGenomes(kids);
+                    //6. Mutate newly created individuals.
+                    kids = mutation(kids);
+                    //6a. repair
+                    kids = repairGenomes(kids);
+                    //7. Create population for t+1
+                    _population.individuals = new PriorityQueue<>(parents);
+                    _population.individuals.addAll(kids);
+
+                    nruns++;
+                }
+            }
         } else {
             //Nothing to do: empty player action
             return new PlayerAction();
@@ -104,13 +113,41 @@ public class OEP extends QMStrategy {
         return bestIndividual();
     }
 
-
-    void generatePopulation()
+    int findPopulationSize(GameState gs)
     {
-        for(int i=0;i<_populationSize;i++)
+        int n =1;
+
+        for(Unit u : gs.getUnits())
+        {
+            if(u.getPlayer() == _playerID)
+            {
+                n*=u.getUnitActions(gs).size();
+            }
+        }
+        return n<_populationSize?n:_populationSize;
+    }
+
+    boolean generatePopulation(GameState gs) throws Exception
+    {
+        boolean choicesThisCycle = genomesGenerator.reset(gs, _playerID);
+
+        if(!choicesThisCycle)
+            return false; //there is no available action for this player
+
+        genomesGenerator.randomizeOrder();
+
+        for(int i=0;i<findPopulationSize(gs);i++)
         {
             //generate new genome and add it to population
+            Genome newGenome = new Genome();
+            newGenome.ID = idGenerator;
+            idGenerator++;
+            
+            newGenome.genes = genomesGenerator.getRandom(newGenome.abstractActions);
+            newGenome.phenotype = gs.cloneIssue(newGenome.genes); //shouldnt I clone the gs?---------ASK
+            _population.individuals.add(newGenome);
         }
+        return true; //there are some available actions for this player
     }
 
     void evaluatePopulation()
@@ -182,6 +219,6 @@ public class OEP extends QMStrategy {
 
     @Override
     public QMStrategy clone() {
-        return new OEP(TIME_BUDGET,ITERATIONS_BUDGET,_populationSize);
+        return new OEP(TIME_BUDGET,ITERATIONS_BUDGET,_populationSize, _lookahead);
     }
 }
